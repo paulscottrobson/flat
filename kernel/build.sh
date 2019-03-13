85d5c9ee0fe466506a1f4bf8e7e22926db586231
Paul Robson <paul@robsons.org.uk>
2019-03-13 17:53:50 +0000

    Update

---
+++ kernel/build.sh
@@ -0,0 +1,16 @@
+#
+#		Tidy up
+#
+rm temp/*
+rm boot.img
+#
+#		Build the bootloader
+#
+pushd ../bootloader
+sh build.sh
+popd
+#
+#		Build the source files
+#
+python ../scripts/processcore.py
+zasm -buw kernel.asm -l kernel.lst -o boot.img
