a6cad6ec8786a1fead0085a56a29d0d1f037347e
paulscottrobson <paulscottrobson@gmail.com>
2019-03-24 18:53:53 +0000

    Update

---
+++ kernel/build.bat
@@ -0,0 +1,17 @@
+@echo off
+rem
+rem		Tidy up
+rem
+del /Q temp\*.*
+del /Q boot.img
+rem
+rem		Build the bootloader
+rem
+pushd ..\bootloader
+call build.bat
+popd
+rem
+rem		Build the source files
+rem
+python ..\scripts\processcore.py
+..\bin\snasm -vice kernel.asm boot.img
--- kernel/run.sh
+++
@@ -1,3 +0,0 @@
-sh build.sh
-python ../scripts/importcode.py standard.flat test.flat
-MONO_IOMAP=all mono ../bin/CSpect.exe -zxnext -cur -brk -exit -w3  ../files/bootloader.sna
