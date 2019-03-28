50b2e75d225edc4adffcf2b83ad7a490053add08
Paul Robson <paul@robsons.org.uk>
2019-03-28 16:15:50 +0000

    Updated for F29

--- kernel/run.sh
+++ kernel/run.sh
@@ -1,3 +1,3 @@
 sh build.sh
-python ../scripts/importcode.py standard.flat test.flat
+python3 ../scripts/importcode.py standard.flat test.flat
 MONO_IOMAP=all mono ../bin/CSpect.exe -zxnext -cur -brk -exit -w3  ../files/bootloader.sna
